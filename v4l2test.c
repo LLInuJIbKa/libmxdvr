0bfbdfbc635c8990acde8826cc8ddc5e1f8e1a29
Ruei-Yuan Lu <RueiYuan.Lu@gmail.com>
2012-05-08 17:29:06 +0800

    Changed demo parameters

--- v4l2test.c
+++ v4l2test.c
@@ -12,8 +12,8 @@
 #define CAPTURE_WIDTH	(640)
 #define CAPTURE_HEIGHT	(360)
 
-#define DISPLAY_WIDTH	(640)
-#define DISPLAY_HEIGHT	(360)
+#define DISPLAY_WIDTH	(1024)
+#define DISPLAY_HEIGHT	(576)
 
 #define FRAME_PER_SECOND	(25)
 
@@ -26,37 +26,36 @@ int main(int argc, char **argv)
 	unsigned char* rgb_buffer = NULL;
 	size_t buffersize;
 	ipu_lib_handle_t* ipu_handle = NULL;
-	char timestring[256] = {};
+	char timestring[256] = { };
 	time_t rawtime;
-	struct tm * timeinfo;
+	struct tm* timeinfo;
 
 	fb_wakeup(TTY_PATH);
 	tty_set_cursor_visible(TTY_PATH, 0);
 
-	rgb_buffer = calloc(1, DISPLAY_WIDTH*DISPLAY_HEIGHT*2);
+	rgb_buffer = calloc(1, DISPLAY_WIDTH * DISPLAY_HEIGHT * 2);
 
 	device = v4l2dev_open(DEVICE_NODE);
 	v4l2dev_init(device, CAPTURE_WIDTH, CAPTURE_HEIGHT, 4);
 
-
 	buffersize = v4l2dev_get_buffersize(device);
+	ipu_query_task();
 	ipu_handle = ipu_init(CAPTURE_WIDTH, CAPTURE_HEIGHT, IPU_PIX_FMT_YUV420P, DISPLAY_WIDTH, DISPLAY_HEIGHT, IPU_PIX_FMT_RGB565, 1);
 
-	text = text_layout_create(200, 30);
-	text_layout_set_font(text, "Liberation Sans", 24);
+	text = text_layout_create(280, 30);
+	text_layout_set_font(text, "Liberation Mono", 24);
 
-
-	for(i=0;i<FRAME_PER_SECOND*60;i++)
+	for(i = 0; i < FRAME_PER_SECOND * 30; ++i)
 	{
 		buffer = v4l2dev_read(device);
-		if(i%FRAME_PER_SECOND == 0)
+		if((i % FRAME_PER_SECOND) == 0)
 		{
 			time(&rawtime);
 			timeinfo = localtime(&rawtime);
 			strftime(timestring, 255, "%p %l:%m:%S %Y/%m/%d", timeinfo);
 			text_layout_render_markup_text(text, timestring);
 		}
-		text_layout_copy_to_yuv420p(text, 460, 300, buffer, CAPTURE_WIDTH, CAPTURE_HEIGHT);
+		text_layout_copy_to_yuv420p(text, 360, 300, buffer, CAPTURE_WIDTH, CAPTURE_HEIGHT);
 		ipu_buffer_update(ipu_handle, buffer, rgb_buffer);
 	}
 	text_layout_destroy(text);
