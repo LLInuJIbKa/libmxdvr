57c1b9c1e4317e5e1d252f8ca43524606cf32015
Ruei-Yuan Lu <RueiYuan.Lu@gmail.com>
2012-09-26 11:19:08 +0800

    Removed keydown detection in fbdemo

--- fbdemo.c
+++ fbdemo.c
@@ -1,36 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <unistd.h>
 #include "fbclient.h"
 #include "mxc_vpu.h"
 
 int main(int argc, char **argv)
 {
-	fd_set fds;
-	struct timeval tv = { 0L, 0L };
-
 	vpu_init();
 	fbclient_start();
 
 	while(1)
 	{
-		usleep(30000);
-
-		/* Detect ENTER key */
-		FD_ZERO(&fds);
-		FD_SET(0, &fds);
-		if(select(1, &fds, NULL, NULL, &tv) > 0) break;
+		sleep(1);
 	}
-
-
-	/* Consume stdin */
-
-	scanf("%*c");
-	fputs("Quiting ...\n", stderr);
-
-	fbclient_stop();
-	vpu_uninit();
 
 
 	return EXIT_SUCCESS;
